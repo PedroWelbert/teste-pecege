916286eb1028d5f2156222c9a2603316197edd79
Pedro Welbert <pedrowelbert@gmail.com>
2022-09-02 01:33:18 -0300

    favicon and header

---
+++ elements/header.php
@@ -0,0 +1,14 @@
+<header>
+  <div class="container">
+    <div class="row">
+      <div class="column">
+        <a href="#" rel="noopener noreferrer">
+          <img src="assets/img/logo.png" alt="Logo" title="Logo Instituto Pay">
+        </a>
+      </div>
+      <div class="column">
+        <?php require_once('elements/nav.php'); ?>
+      </div>
+    </div>
+  </div>
+</header>
---
+++ elements/nav.php
@@ -0,0 +1,28 @@
+<nav>
+  <ul class="menu">
+    <li class="menu-item">
+      <a href="#home">
+        <span>01.</span>
+        <span>Home</span>
+      </a>
+    </li>
+    <li class="menu-item">
+      <a href="#whoUses">
+        <span>02.</span>
+        <span>Quem usa?</span>
+      </a>
+    </li>
+    <li class="menu-item">
+      <a href="#highlights">
+        <span>03.</span>
+        <span>Diferenciais</span>
+      </a>
+    </li>
+    <li class="menu-item">
+      <a href="#contact">
+        <span>04.</span>
+        <span>Contato</span>
+      </a>
+    </li>
+  </ul>
+</nav>
