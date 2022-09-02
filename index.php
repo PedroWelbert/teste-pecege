f486cdf419fe0adcb5b5ce2d353ea6d4a2b4f6ac
Pedro Welbert <pedrowelbert@gmail.com>
2022-09-02 16:34:52 -0300

    home

--- index.php
+++ index.php
@@ -3,5 +3,8 @@
 <?php require_once('elements/head.php'); ?>
 <body>
   <?php require_once('elements/header.php'); ?>
+  <main>
+    <?php require_once('parts/home.php'); ?>
+  </main>
 </body>
 </html>
---
+++ parts/home.php
@@ -0,0 +1,37 @@
+<section id="home">
+  <div class="container">
+    <div class="row one">
+      <div class="column">
+        <h1>
+          Lorem ipsum dolor sit amet consectetur Elit. Fusce ut porta leo.
+        </h1>
+        <a href="#whoUses">
+          Saiba mais
+        </a>
+      </div>
+    </div>
+    <div class="row two">
+      <div class="column">
+        <span>01.</span>
+        <h2>Tudo começa com uma marca forte.</h2>
+        <div class="info">
+          <div class="column">
+            <h4>Pagamentos</h4>
+          </div>
+          <div class="column description">
+            <p>Fusce ut porta leo. Sed iaculis lupus augue cursus, sodales euismod vitae porttitor. Suspendisse sed nunc.</p>
+            <a href="#contact">
+              <span>Entre em contato</span>
+              <?php require_once('./assets/img/icon/arrow-right.svg'); ?>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <figure>
+          <img src="./assets/img/mobile_banner.png" alt="">
+        </figure>
+      </div>
+    </div>
+  </div>
+</section>
