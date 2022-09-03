b682a0b20fc6ed46189c274cc2b4d8148d758ba9
Pedro Welbert <pedrowelbert@gmail.com>
2022-09-03 02:02:12 -0300

    contact

--- parts/contact.php
+++ parts/contact.php
@@ -1,3 +1,29 @@
 <section id="contact">
-  
+  <div class="container">
+    <div class="row two">
+      <div class="column">
+        <span>04.</span>
+        <h2>Tudo na nuvem.</h2>
+        <div class="info">
+          <div class="column">
+            <h4>Cloud Safe</h4>
+          </div>
+          <div class="column description">
+            <p>Quisque eget velit. Duis vitae semper augue. Orci nulla magna, arcu diam inceptos lupus ut quis nibh eu.</p>
+            <a href="#contact">
+              <span>Cadastre-se</span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15">
+                <path id="arrow-right" d="M7.3,34.7l-.663.671a.41.41,0,0,0,0,.575l5.194,5.255H.4a.4.4,0,0,0-.4.406v.948a.4.4,0,0,0,.4.406H11.834L6.64,48.221a.41.41,0,0,0,0,.575l.663.671a.4.4,0,0,0,.568,0l7.012-7.094a.41.41,0,0,0,0-.575L7.871,34.7A.4.4,0,0,0,7.3,34.7Z" transform="translate(0 -34.586)"/>
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="column">
+        <figure>
+          <img src="./assets/img/mobile_cloud.png" alt="">
+        </figure>
+      </div>
+    </div>
+  </div>
 </section>
--- parts/home.php
+++ parts/home.php
@@ -22,7 +22,9 @@
             <p>Fusce ut porta leo. Sed iaculis lupus augue cursus, sodales euismod vitae porttitor. Suspendisse sed nunc.</p>
             <a href="#contact">
               <span>Entre em contato</span>
-              <?php require_once('./assets/img/icon/arrow-right.svg'); ?>
+              <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15">
+                <path id="arrow-right" d="M7.3,34.7l-.663.671a.41.41,0,0,0,0,.575l5.194,5.255H.4a.4.4,0,0,0-.4.406v.948a.4.4,0,0,0,.4.406H11.834L6.64,48.221a.41.41,0,0,0,0,.575l.663.671a.4.4,0,0,0,.568,0l7.012-7.094a.41.41,0,0,0,0-.575L7.871,34.7A.4.4,0,0,0,7.3,34.7Z" transform="translate(0 -34.586)"/>
+              </svg>
             </a>
           </div>
         </div>
