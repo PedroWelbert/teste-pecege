1440afaf6c3eac911bc29ceba814564500a3d6f6
Pedro Welbert <pedrowelbert@gmail.com>
2022-09-03 03:30:06 -0300

    footer

---
+++ elements/footer.php
@@ -0,0 +1,101 @@
+<footer>
+  <div class="logo">
+    <img src="./assets/img/logo_white.png" alt="">
+  </div>
+  <div class="container">
+    <div class="row">
+      <div class="column">
+        <h4>Sobre nós</h4>
+        <ul class="menu">
+          <li class="menu-item">
+            <a href="#" target="_blank" rel="noopener noreferrer">Lorem Ipsum</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" target="_blank" rel="noopener noreferrer">Lorem Ipsum</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" target="_blank" rel="noopener noreferrer">Lorem Ipsum</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" target="_blank" rel="noopener noreferrer">Lorem Ipsum</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" target="_blank" rel="noopener noreferrer">Lorem Ipsum</a>
+          </li>
+        </ul>
+      </div>
+      <div class="column">
+        <h4>Links</h4>
+        <ul class="menu">
+          <li class="menu-item">
+            <a href="#" target="_blank" rel="noopener noreferrer">Lorem Ipsum</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" target="_blank" rel="noopener noreferrer">Lorem Ipsum</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" target="_blank" rel="noopener noreferrer">Lorem Ipsum</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" target="_blank" rel="noopener noreferrer">Lorem Ipsum</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" target="_blank" rel="noopener noreferrer">Lorem Ipsum</a>
+          </li>
+        </ul>
+      </div>
+      <div class="column">
+        <h4>Categorias</h4>
+        <ul class="menu">
+          <li class="menu-item">
+            <a href="#" target="_blank" rel="noopener noreferrer">Lorem Ipsum</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" target="_blank" rel="noopener noreferrer">Lorem Ipsum</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" target="_blank" rel="noopener noreferrer">Lorem Ipsum</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" target="_blank" rel="noopener noreferrer">Lorem Ipsum</a>
+          </li>
+          <li class="menu-item">
+            <a href="#" target="_blank" rel="noopener noreferrer">Lorem Ipsum</a>
+          </li>
+        </ul>
+      </div>
+      <div class="column">
+        <h4>NewsLetter</h4>
+        <form action="">
+          <label for="email">E-mail</label>
+          <div class="content">
+            <input type="text" placeholder="Digite aqui seu e-mail">
+            <a href="#contact" type="submit">
+              <span>Enviar</span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16">
+                <path id="paper-plane" d="M14.886.07.4,8.425a.75.75,0,0,0,.069,1.35l3.322,1.393L12.77,3.257a.187.187,0,0,1,.269.259L5.51,12.686V15.2a.75.75,0,0,0,1.328.494L8.823,13.28l3.894,1.631a.752.752,0,0,0,1.031-.569L16,.845A.75.75,0,0,0,14.886.07Z" transform="translate(-0.01 0.031)" fill="#0180ff"/>
+              </svg>
+            </a>
+          </div>
+        </form>
+        <h4>Baixe o app</h4>
+        <div class="buttons">
+          <a href="#">
+            <figure>
+              <img src="assets/img/google_play.png" alt="">
+            </figure>
+          </a>
+          <a href="#">
+            <figure>
+              <img src="assets/img/app_store.png" alt="">
+            </figure>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="copyright">
+      <p>Todos os direitos reservados - <?= date('Y') ?></p>
+      <p>Instituto Pay</p>
+    </div>
+  </div>
+</footer>
